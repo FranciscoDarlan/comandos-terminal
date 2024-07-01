b224b9c8ce703bf7c5083902385e0d0925413a74
Francisco Darlan <118319737+FranciscoDarlan@users.noreply.github.com>
2024-07-01 14:17:50 -0300

    codigos terminal comandos

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
+COMANDOS NO TERMINAL: powershel
+
+ls - lista todas as pastas
+clear - limpa
+cd - navegar/entra na pasta
+cd nomedapasta - entra na pasta
+cd .. - volta
+
+mkdir - cria uma pasta
+rmdir - deleta uma pasta
+
+
+_______________________________________________________________________________________________
+EXEMPLO:
+
+quero criar uma pasta na area de trabalho:
+cd desktop
+npm create vite!latest
+
+pergunta ? quer instalar o crate vite ? y
+  
+qual nome do projeto? 
+  devclub-cadastro-usuarios
+  
+Qual ferramenta?
+  react
+  vue
+  sbel
+
+quer o react com typescript ou javascript ?
+  javascript
+
+agora se eu der um :
+ls- consigo enxergar a pasta criada
+
+para abrir essa pasta no vscode vou digitar : -> atalho
+code dev+tab - automaticamente ele procura algo parecido e completa!
+code .\devclub-cadastro-usuarios\            ok
+
+JA VAI ABRIR NO VSCODE COM TODOS OS ARQUIVOS CRIADOS!
 
